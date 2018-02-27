6f80eefe25d03d0e3db6c59917b72a7dcf582e6b
yangyanjie <920526047@qq.com>
2018-02-27 11:28:45 +0800

    初始化

--- src/views/activity/member-day/member-day.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Flex from 'assets/js/flex.js'
-import App from './member-day.vue'
-new Vue({
-    render: h => h(App)
-}).$mount('#app')
--- src/views/activity/system-service/system-service.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Flex from 'assets/js/flex.js'
-import App from './system-service.vue'
-
-new Vue({
-    render: h => h(App)
-}).$mount('#app')
